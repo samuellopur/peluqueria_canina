e41842eb31f0f8afe5f52700aeee963f33cfc9fa
Samuel Lopez <samuellopez01@hotmail.com>
2025-02-26 22:45:57 -0500

    Se agrega campos y etiquetas al form LoadData

--- src/main/java/org/mycompany/peluqueriacanina/igu/LoadData.java
+++ src/main/java/org/mycompany/peluqueriacanina/igu/LoadData.java
@@ -1,4 +1,14 @@
 package org.mycompany.peluqueriacanina.igu;
 
+import javax.swing.*;
+
 public class LoadData {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextArea textArea1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
 }
